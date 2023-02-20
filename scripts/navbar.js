d067e7e6827d1bf082c61066dc6020708b7b34db
LightAgent <65326874+LightAgent@users.noreply.github.com>
2023-02-20 06:40:12 +0200

    navbar fix

---
+++ scripts/navbar.js
@@ -0,0 +1,8 @@
+// javascript to make side menu appear 
+function openNav() {
+    document.getElementById("sidenav").style.width = "50%";
+}
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0%";
+}
+closeNav()
